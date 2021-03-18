cb785edfdacc1067057ede4c63d71387f3ddf7fe
Yash Jaiswal <ys432526@dal.ca>
2021-03-18 00:18:25 -0300

    updated mailFormat for tutorapplication submission

--- tutorpoint-nodeserver/api/controllers/mailController.js
+++ tutorpoint-nodeserver/api/controllers/mailController.js
@@ -1,3 +1,4 @@
+/* mail style adapted from nodemailer package documentation*/
 const mongoose = require("mongoose");
 const nodemailer = require("nodemailer");
 
@@ -8,6 +9,7 @@ const mailSender = (req, res) => {
   const courseId = req.body.formData.course_id;
   const courseName = req.body.formData.course;
   const description = req.body.formData.description;
+  const availability = req.body.formData.availability;
   const fileNames = req.body.fileNames;
   const fileDestination = req.body.fileDestination;
   const tutorApplicationId = req.body.tutorApplicationId;
@@ -54,13 +56,21 @@ const mailSender = (req, res) => {
       //   studentMail +
       //   "for futher information. \n Please make a decision on this application by clicking   ", // plain text body
       html:
-        "<h3>Respected Faculty,</h3><br><h4>This is regarding an application submitted the by student " +
+        "<h3>Respected Faculty,</h3><br><h4>This is regarding an application submitted by the student " +
         studentName +
         " to tutor for one of your courses " +
         courseName +
         " - " +
         courseId +
-        " <br> Please go through the attached documents and the application to make a decision. <br> Also, you can directly contact the student at " +
+        ".<br> <br>Application Details<br> ----------------- <br> Student Name: " +
+        studentName +
+        "<br> E-mail: " +
+        studentMail +
+        "<br> Message: " +
+        description +
+        "<br> Availability: " +
+        availability +
+        "<br> <br> Please go through the attached documents and the application to make a decision. <br> Also, you can directly contact the student at " +
         studentMail +
         "for futher information. <br> <b>Please make a decision on this application by clicking <a href='http://localhost:3000/#/manage-tutor-application'>Here</a></b> </h4> ", // html body
       attachments: attachmentsMail,
