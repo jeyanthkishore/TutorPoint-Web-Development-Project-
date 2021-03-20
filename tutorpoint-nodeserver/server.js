6bfbbd34f36bd55c2db6010e922828c2970c4739
Yash Jaiswal <ys432526@dal.ca>
2021-03-20 01:34:26 -0300

    adding references for code

--- tutorpoint-nodeserver/server.js
+++ tutorpoint-nodeserver/server.js
@@ -25,7 +25,7 @@ app.use("/api/user", userRoute);
 app.use("/api/mail", mailRoute);
 app.use("/api/courses", courseRoute);
 app.use("/api/tutorDetails", tutorRoute);
-app.use("/api/workshopDetails",workshopRoute);
+app.use("/api/workshopDetails", workshopRoute);
 app.use("/", loginRoute);
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/build/index.html"));
